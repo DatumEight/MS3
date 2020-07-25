0c69400c491b4cc9b6d1e6c029a65eb4dbae9d2b
Datum <allen.james.west@gmail.com>
2020-07-25 22:06:36 +0000

    added stylesheet link and got rid of css.min

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 Milestone 3 project for TCI 
 
+#The Grand Archive
+
 #ideas 
     -LITRpg audiobook review and recommendation website
     -user can log in and save books to a list for later purchase potentially through an affiliate link
--- flask/run.py
+++ flask/run.py
@@ -5,19 +5,22 @@ app = Flask(__name__)
 
 
 @app.route("/")
-
 def index():
     return render_template("index.html")
 
+@app.route("/about")
 def about():
     return render_template("about.html")
 
+@app.route("/blog")
 def blog():
     return render_template("blog.html")
 
+@app.route("/login")
 def login():
     return render_template("login.html")
 
+@app.route("/register")
 def register():
     return render_template("register.html")
 
