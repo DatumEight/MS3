01561de39823c6a6fd8d41d438fc9e6c25246af3
Datum <allen.james.west@gmail.com>
2020-07-25 21:38:20 +0000

    added routing for all pages and started block content

---
+++ run.py
@@ -0,0 +1,35 @@
+import os
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+
+def index():
+    return render_template("index.html")
+
+def about():
+    return render_template("about.html")
+
+def blog():
+    return render_template("blog.html")
+
+def login():
+    return render_template("login.html")
+
+def register():
+    return render_template("register.html")
+
+
+
+
+if __name__ == '__main__':
+    app.run(host=os.environ.get("IP"),
+            port=int(os.environ.get("PORT")),
+            debug=True)
+
+
